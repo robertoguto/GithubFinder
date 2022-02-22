9722fe5c62f823aab10e24dc09c22b27e7c63b78
Roberto Augusto <roberto.augusto@krooze.com.br>
2022-02-21 22:33:01 -0300

    Add component finder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FinderComponent } from './finder/finder.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FinderComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/finder/finder.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FinderComponent } from './finder.component';
+
+@NgModule({
+  declarations: [
+    FinderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FinderModule { }
